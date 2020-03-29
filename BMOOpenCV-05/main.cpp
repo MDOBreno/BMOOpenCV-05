e6c700e4cf272b633b763830635b42dbe8f84da3
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-03-29 14:19:05 -0300

    We take a quick look at splitting an image into its component channels and merging them back together. This operation becomes very important later when you need to construct custom Mat objects for operations like the Fourier transform.

--- BMOOpenCV-05/main.cpp
+++ BMOOpenCV-05/main.cpp
@@ -6,10 +6,39 @@
 //  Copyright © 2020 ProgramasBMO. All rights reserved.
 //
 
-#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    String caminho = "/Users/brenomedeiros/Documents/ProgramasBMO/Cpp/BMOOpenCV-05/BMOOpenCV-05/";
+    String caminhoImagens = "/Users/brenomedeiros/Documents/ProgramasBMO/Cpp/BMOOpenCV-05/BMOOpenCV-05/Images/";
+    
+    Mat original = imread(caminhoImagens+"Me.jpeg", CV_LOAD_IMAGE_COLOR);
+    
+    Mat canaisDivididos[3];
+    split(original, canaisDivididos);
+    
+    namedWindow("Azul", CV_WINDOW_AUTOSIZE);
+    imshow("Azul", canaisDivididos[0]);
+    moveWindow("Azul", 0*(original.cols/2), 1080);
+    namedWindow("Verde", CV_WINDOW_AUTOSIZE);
+    imshow("Verde", canaisDivididos[1]);
+    moveWindow("Verde", 1*(original.cols/2), 1080);
+    namedWindow("Vermelho", CV_WINDOW_AUTOSIZE);
+    imshow("Vermelho", canaisDivididos[2]);
+    moveWindow("Vermelho", 2*(original.cols/2), 1080);
+    
+    //Substitui("apaga") a matriz do Vermelho pro uma de mesma dimensao e tamanho de pixel.
+    canaisDivididos[2] = Mat::zeros(canaisDivididos[2].size(), CV_8UC1);
+    Mat saida;
+    merge(canaisDivididos, 3, saida);
+    
+    imshow("Saida", saida);
+    
+    waitKey();
+    
+
     return 0;
 }
